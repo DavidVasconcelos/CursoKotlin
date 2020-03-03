14e50b837a3340ea7c1a45a517a642625b5ce559
DavidVasconcelos <dsv10@hotmail.com>
2020-03-02 22:14:28 -0300

    adicionado o fold

--- src/lambdas/Map2.kt
+++ src/lambdas/Map2.kt
@@ -14,4 +14,26 @@ fun main(args: Array<String>) {
     var precoTotal = materialEscolar.map { it.preco }.reduce(totalizar) // o primeiro parametro é o acumulador e o segundo elemento é o valor que está sendo iterado
 
     println("O preço médio é R$ ${precoTotal / materialEscolar.size}.")
+
+    val reduceTotal = materialEscolar.map { it.preco }.reduce {
+
+            acumulador, valor ->
+        println("Accumulador -> $acumulador e o valor -> $valor")
+        totalizar(acumulador, valor)
+    }
+
+    println("Com reduce o total é R$ $reduceTotal.")
+
+    val fold = 10.0
+
+    println("Com fold em $fold")
+
+    val foldTotal = materialEscolar.map { it.preco }.fold(fold) {
+
+            acumulador, valor ->
+        println("Accumulador -> $acumulador e o valor -> $valor")
+        totalizar(acumulador, valor)
+    }
+
+    println("O fold o total é R$ $foldTotal.")
 }
